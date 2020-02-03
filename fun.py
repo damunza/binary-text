2e7568ba36d232cafd5bf1bde36f96d5296b51ea
damunza <danidamunza@gmail.com>
2020-02-03 22:45:40 +0300

    corrected most errors set up git so far it is presenting workeable data

---
+++ fun.py
@@ -0,0 +1,30 @@
+def chartotext(x):
+    '''
+    a function that converts the processe characters into text codes 
+    '''
+    letters = []
+    for element in x:
+            # this separates the individual character values intro single digits
+        val = 0
+        for one in element:
+            val += int(one) * (2**(element.find(one)))
+            print(one)
+            letters.append(val)
+    return letters
+
+def bintotext(a):
+    '''
+    a function that takes in a list of numbers and returns its character code
+    '''
+    characters = []
+    for item in a :
+        work = str(item)[3:]
+        b = work[::-1]
+        # up to here each number in the series is reveresed and only represent characters
+        characters.append(b)
+        # a list of all the bionarys that have already been processed 
+    return characters
+                 
+x = [1001010, 1101111]
+y = bintotext(x)
+print(y)
